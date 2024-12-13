565c94d6e59daba450e00429562a68264a64f405
Николай Казанцев <kazancev_nic@mail.ru>
2024-12-13 11:08:01 +0700

    Правка.

---
+++ src/model/destinations-and-offers-model.js
@@ -0,0 +1,24 @@
+import { getDestinations } from '../mock/destinations';
+import { getOffers } from '../mock/offers';
+
+
+export default class TripModel {
+  #offers = [];
+  #destinations = [];
+
+  //инициализация тестовых данных
+  init() {
+    this.#offers = getOffers();
+    this.#destinations = getDestinations();
+  }
+
+  //получить массив дополнительных предложений
+  getOffers() {
+    return this.#offers;
+  }
+
+  //получить массив пунктов назначения
+  getDestinations() {
+    return this.#destinations;
+  }
+}
